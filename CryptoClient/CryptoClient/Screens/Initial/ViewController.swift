69e3832c0d9badc01c917bb788d9f6df40234e73
bpamukcu <bpamukcu@anadolubank.com.tr>
2022-09-29 16:31:11 +0300

    Ios project added.

---
+++ CryptoClient/CryptoClient/Screens/Initial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoClient
+//
+//  Created by Batuhan PAMUKÇU (Mobil Uygulama Geliştirme-Yazılım Uzmanı III) on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
